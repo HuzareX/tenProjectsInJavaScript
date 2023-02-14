ca20460b5a89f60cb5361e3a871edda57a3bc2fc
HuzareX <danielwegierski@gmail.com>
2023-02-14 20:27:24 +0100

    drinkFinder

---
+++ drinkFinder/script.js
@@ -0,0 +1,19 @@
+const searchInput = document.querySelector('.search')
+const li = document.querySelectorAll('li')
+
+const searchEngine = e => {
+	const text = e.target.value.toLowerCase()
+
+
+	li.forEach(el => {
+
+
+		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = 'block'
+		} else {
+			el.style.display = 'none'
+		}
+	})
+}
+
+searchInput.addEventListener('keyup', searchEngine)
